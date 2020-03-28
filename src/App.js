f438f9ac4ac4c92b3b3591e9eebf37861b657c24
Lola <lola.donval@gmail.com>
2020-03-28 21:28:29 +0100

    create ContactList component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import Contact from './components/Contact'
+import ContactList from './components/ContactList'
 
 function App() {
   return (
     <div className="App">
-      <Contact 
-        name="Mark Kennedy"
-        avatar="https://randomuser.me/api/portraits/men/17.jpg"
-        online={true}
-      />
-      <Contact 
-        name="Denise Peters"
-        avatar="https://randomuser.me/api/portraits/women/49.jpg"
-        online={false}
-      />
-      <Contact 
-        name="Jacqueline Hicks"
-        avatar="https://randomuser.me/api/portraits/women/3.jpg"
-        online={true}
-      />
+      <ContactList />
     </div>
   );
 }
